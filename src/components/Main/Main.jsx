4e076af6f79ea86ae310e904e7ca8ea0377d9eff
Mohsen Bakhshandeh <waltonmohsen@gmail.com>
2022-12-20 15:48:33 +0330

    digging mohghol shit code

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import './main.css'
+import { useEffect, useState } from 'react'
+import './Main.css'
 
 function Main(){
+    const [list, setList] = useState([])
+
+    const handleChange = () => {
+        const input_text = document.getElementById('todo-text').value
+        setList([...list, input_text])
+    }
+
     return (
             <div className='Main'>
                 <div className='head'>
                     <p>TO DO LIST</p>
                 </div>
                 <div className='inBtn'>
-                    <input className='inpot' type="text" placeholder='HowUdOin'/>
-                    <button className='btn'>Add</button>
+                    <input className='inpot' id='todo-text' type="text" placeholder='HowUdOin'/>
+                    <button className='btn' onClick={() => handleChange()}>Add</button>
                 </div>
                 <div className='list'>
                     <ol className='ol-list'>
-                        <li>sallam</li>
-                        <li>sallam</li>
-                        <li>sallam</li>
-                        <li>sallam</li>
+                        {list.map((item, index) => {
+                            return <li key={index}>{item}</li>
+                        })}
                     </ol>
                 </div>
             </div>
